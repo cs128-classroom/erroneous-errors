bc62737e08e09770b8dbaca02f3d881b828ac482
sydneyrcasey <sydneyrcasey@yahoo.com>
2026-02-18 23:38:51 +0000

    initial commit

--- src/driver.cc
+++ src/driver.cc
@@ -3,7 +3,11 @@
 #include "solution.hpp"
 
 int main() {
-  std::cout << "Hello! This compiles ok." << std::endl;
-
+  std::vector<Student> students = ReadCSVFile("./testing_files/example.csv");
+  WriteGradeStatistics("./testing_files/actual_output.txt", students);
+  for (const Student& s : students) {
+    WriteGradeReport(
+        "./testing_files/actual_students_output/" + s.full_name + ".txt", s);
+  }
   return 0;
 }
